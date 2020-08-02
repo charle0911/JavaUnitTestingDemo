4809aa8201043f442e9072d9aef7d6d7df350ca7
Charlie Lee <charle0911@gmail.com>
2020-08-02 20:34:18 +0800

    Add jacoco

---
+++ src/test/java/com/demo/teach/assertj/AssertJExample.java
@@ -0,0 +1,18 @@
+package com.demo.teach.assertj;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class AssertJExample {
+
+    @Test
+    public void testHas() {
+    }
+
+}
+
+class TestObject {
+    String name;
+    String type;
+}
